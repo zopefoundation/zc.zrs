9001dd32669f93d4ec155c1248b1456bbdb3f174
Jim Fulton <jim@zope.com>
2007-04-01 17:48:10 +0000

    Reorganized and added producer suspend/resume test.
    Fixed a bug in test setup that prevented logging except for primary.

--- src/zc/zrs/tests.py
+++ src/zc/zrs/tests.py
@@ -26,6 +26,174 @@ import twisted.python.failure
 import zc.zrs.fsiterator
 import zc.zrs.sizedmessage
 
+
+def scan_from_back():
+    r"""
+Create the database:
+
+    >>> import ZODB.FileStorage
+    >>> from ZODB.DB import DB
+    >>> import persistent.dict
+
+    >>> fs = ZODB.FileStorage.FileStorage('Data.fs')
+    >>> db = DB(fs)
+    >>> conn = db.open()
+
+    >>> for i in range(100):
+    ...     conn.root()[i] = persistent.dict.PersistentDict()
+    ...     commit()
+
+Now, be evil, and muck up the beginning: :)
+
+    >>> fs._file.seek(12)
+    >>> fs._file.write('\xff'*8)
+    >>> conn.root()[100] = persistent.dict.PersistentDict()
+    >>> commit()
+
+If we try to iterate from the beginning, we'll get an error:
+
+    >>> condition = threading.Condition()
+    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition)
+    >>> it.next()
+    Traceback (most recent call last):
+    ...
+    CorruptedDataError: Error reading unknown oid.  Found '' at 4
+
+    >>> def tid_from_time(t):
+    ...     return repr(TimeStamp(*(time.gmtime(t)[:5] + (t%60,))))
+
+    >>> tid = tid_from_time(time.time()-70)
+    >>> zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
+    Traceback (most recent call last):
+    ...
+    OverflowError: long too big to convert
+
+But, if we iterate from near the end, we'll be OK:
+
+    >>> tid = tid_from_time(time.time()-30)
+    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
+    >>> trans = it.next()
+    >>> from ZODB import utils
+    >>> print TimeStamp(trans.tid), [utils.u64(r.oid) for r in trans]
+    2007-03-21 20:34:09.000000 [0L, 72L]
+
+    >>> print TimeStamp(tid)
+    2007-03-21 20:34:08.000000
+
+    >>> tid = tid_from_time(time.time()-29.5)
+    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
+    >>> trans = it.next()
+    >>> from ZODB import utils
+    >>> print TimeStamp(trans.tid), [utils.u64(r.oid) for r in trans]
+    2007-03-21 20:34:09.000000 [0L, 72L]
+
+    >>> print TimeStamp(tid)
+    2007-03-21 20:34:08.500000
+
+    """
+
+def primary_suspend_resume():
+    """
+The primary producer is supposed to be suspendable.
+
+We'll create a file-storage:
+
+    >>> import ZODB.FileStorage
+    >>> fs = ZODB.FileStorage.FileStorage('Data.fs')
+    >>> from ZODB.DB import DB
+    >>> db = DB(fs)
+
+Now, we'll create an iterator:
+
+    >>> import zc.zrs.fsiterator
+    >>> iterator = zc.zrs.fsiterator.FileStorageIterator(fs)
+
+And a special transport that will output data when it is called:
+
+    >>> class Reactor:
+    ...     def callFromThread(self, f, *args, **kw):
+    ...         f(*args, **kw)
+
+    >>> class Transport:
+    ...     def __init__(self):
+    ...         self.reactor = Reactor()
+    ...     def write(self, message):
+    ...         message = message[4:] # cheat. :)
+    ...         print cPickle.loads(message)[0]
+    ...     def registerProducer(self, producer, streaming):
+    ...         print 'registered producer'
+    ...     def unregisterProducer(self):
+    ...         print 'unregistered producer'
+    ...     def loseConnection(self):
+    ...         print 'loseConnection'
+
+And a producer based on the iterator and transport:
+
+    >>> import zc.zrs.primary
+    >>> import time
+    >>> producer = zc.zrs.primary.PrimaryProducer(iterator, Transport(), 'test'
+    ...            ); time.sleep(0.1)
+    registered producer
+    T
+    S
+    C
+
+We get the initial transaction, because the producer starts producing
+immediately.  Let's oause producing:
+
+    >>> producer.pauseProducing()
+
+and we'll create another transaction:
+
+    >>> conn = db.open()
+    >>> ob = conn.root()
+    >>> import persistent.dict
+    >>> ob.x = persistent.dict.PersistentDict()
+    >>> commit()
+    >>> iterator.notify()
+    >>> ob = ob.x
+    >>> ob.x = persistent.dict.PersistentDict()
+    >>> commit()
+    >>> iterator.notify()
+    >>> time.sleep(0.1)
+    
+No output because we are paused.  Now let's resume:
+
+    >>> producer.resumeProducing(); time.sleep(0.1)
+    T
+    S
+    S
+    C
+    T
+    S
+    S
+    C
+
+and pause again:
+
+    >>> producer.pauseProducing()
+    >>> ob = ob.x
+    >>> ob.x = persistent.dict.PersistentDict()
+    >>> commit()
+    >>> iterator.notify()
+    >>> time.sleep(0.1)
+
+and resume:
+
+    >>> producer.resumeProducing(); time.sleep(0.1)
+    T
+    S
+    S
+    C
+
+    >>> producer.close()
+    unregistered producer
+    loseConnection
+
+    >>> db.close()
+
+"""
+
 class TestReactor:
 
     def __init__(self):
@@ -171,78 +339,11 @@ def setUp(test):
 
     test.globs['reactor'] = TestReactor()
 
-    logger = logging.getLogger('zc.zrs.primary')
+    logger = logging.getLogger('zc.zrs')
     logger.setLevel(1)
     setupstack.register(test, logger.setLevel, 0)
     logger.addHandler(stdout_handler)
     setupstack.register(test, logger.removeHandler, stdout_handler)
-
-
-def scan_from_back():
-    r"""
-Create the database:
-
-    >>> import ZODB.FileStorage
-    >>> from ZODB.DB import DB
-    >>> import persistent.dict
-
-    >>> fs = ZODB.FileStorage.FileStorage('Data.fs')
-    >>> db = DB(fs)
-    >>> conn = db.open()
-
-    >>> for i in range(100):
-    ...     conn.root()[i] = persistent.dict.PersistentDict()
-    ...     commit()
-
-Now, be evil, and muck up the beginning: :)
-
-    >>> fs._file.seek(12)
-    >>> fs._file.write('\xff'*8)
-    >>> conn.root()[100] = persistent.dict.PersistentDict()
-    >>> commit()
-
-If we try to iterate from the beginning, we'll get an error:
-
-    >>> condition = threading.Condition()
-    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition)
-    >>> it.next()
-    Traceback (most recent call last):
-    ...
-    CorruptedDataError: Error reading unknown oid.  Found '' at 4
-
-    >>> def tid_from_time(t):
-    ...     return repr(TimeStamp(*(time.gmtime(t)[:5] + (t%60,))))
-
-    >>> tid = tid_from_time(time.time()-70)
-    >>> zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
-    Traceback (most recent call last):
-    ...
-    OverflowError: long too big to convert
-
-But, if we iterate from near the end, we'll be OK:
-
-    >>> tid = tid_from_time(time.time()-30)
-    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
-    >>> trans = it.next()
-    >>> from ZODB import utils
-    >>> print TimeStamp(trans.tid), [utils.u64(r.oid) for r in trans]
-    2007-03-21 20:34:09.000000 [0L, 72L]
-
-    >>> print TimeStamp(tid)
-    2007-03-21 20:34:08.000000
-
-    >>> tid = tid_from_time(time.time()-29.5)
-    >>> it = zc.zrs.fsiterator.FileStorageIterator(fs, condition, tid)
-    >>> trans = it.next()
-    >>> from ZODB import utils
-    >>> print TimeStamp(trans.tid), [utils.u64(r.oid) for r in trans]
-    2007-03-21 20:34:09.000000 [0L, 72L]
-
-    >>> print TimeStamp(tid)
-    2007-03-21 20:34:08.500000
-
-    """
-
 
 def test_suite():
     return unittest.TestSuite((
