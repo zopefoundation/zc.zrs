1c5182f9f3341a0c608b11d395c728beeec151c6
Jim Fulton <jim@zope.com>
2007-04-19 17:50:04 +0000

    Initial evaluation dist builder.

---
+++ eval.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+open('setup.cfg', 'w').write("""
+[bdist_egg]
+exclude-source-files = true
+""")
+
+open('LICENSE.txt', 'w').write(open('ZEL.txt').read())
+
+try:
+    setup(
+        name = 'zc.zrs_eval',
+        version = open('version.txt').read().strip(),
+        author = "Jim Fulton",
+        author_email = "jim#zope.com",
+        description = "Zope Replication Server",
+        license = "Zope Evaluation License 1.0",
+        keywords = "ZODB",
+
+        packages = ['zc', 'zc.zrs'],
+        include_package_data = True,
+        data_files = [('.', ['README.txt', 'LICENSE.txt'])],
+        zip_safe = True,
+        package_dir = {'':'src'},
+        namespace_packages = ['zc'],
+        install_requires = ['setuptools', 'ZODB3', 'zc.twisted'],
+        )
+finally:
+    os.remove('setup.cfg')
+    os.remove('LICENSE.txt')
