d4be3e9c661a382bed520ecabfd9ffd6f24c80c7
Jim Fulton <jim@zope.com>
2009-06-04 19:25:32 +0000

    checkpoint

--- eval/setup.py
+++ eval/setup.py
@@ -19,18 +19,10 @@ import os
 import random
 import time
 
-open('setup.cfg', 'w').write("""
-[bdist_egg]
-exclude-source-files = true
-""")
-
-open('LICENSE.txt', 'w').write(open('ZEL.txt').read())
-
 entry_points = """
 [console_scripts]
 zrsmonitor-script = zc.zrs.monitor:main
 """
-
 
 version = open('zrsversion.cfg').read().strip().split()[-1]+'.eval'
 version += datetime.date.today().isoformat().replace('-', '')
@@ -56,7 +48,6 @@ options = dict(
 
     packages = ['zc', 'zc.zrs'],
     include_package_data = True,
-    data_files = [('.', ['README.txt', 'LICENSE.txt'])],
     zip_safe = True,
     entry_points = entry_points,
     package_dir = {'':'src'},
@@ -64,18 +55,14 @@ options = dict(
     install_requires = ['setuptools', 'ZODB3', 'Twisted'],
     )
 
-try:
-    setup(**options)
+setup(**options)
 
-    deadline = random.randint(40, 60)
-    deadline = time.time()+(86400*deadline)
-    deadline = timeout_template % deadline
-    for name in 'primary', 'reactor', 'secondary', 'sizedmessage':
-        open(os.path.join('build', 'lib', 'zc', 'zrs', name+'.py'), 'a').write(
-            deadline)
+deadline = random.randint(40, 60)
+deadline = time.time()+(86400*deadline)
+deadline = timeout_template % deadline
+for name in 'primary', 'reactor', 'secondary', 'sizedmessage':
+    open(os.path.join('build', 'lib', 'zc', 'zrs', name+'.py'), 'a').write(
+        deadline)
 
-    setup(**options)
-finally:
-    os.remove('setup.cfg')
-    os.remove('LICENSE.txt')
+setup(**options)
 
