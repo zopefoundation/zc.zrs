18d3ca76c0f9985cdb8ff9ed14ce0ce344c67a02
Jim Fulton <jim@zope.com>
2010-06-04 14:55:57 +0000

    Backported eval timeout.

--- eval.py
+++ eval.py
@@ -1,10 +1,26 @@
-import os
 from setuptools import setup
+import datetime
+import os
+import random
+import time
 
-open('setup.cfg', 'w').write("""
-[bdist_egg]
-exclude-source-files = true
-""")
+entry_points = """
+[console_scripts]
+zrsmonitor-script = zc.zrs.monitor:main
+"""
+
+version = open('version.txt').read().strip()+'.eval'
+version += datetime.date.today().isoformat().replace('-', '')
+
+timeout_template="""
+
+if __import__('time').time() > %s:
+    raise SystemError('Your ZRS evaluation has expired.')
+"""
+
+import shutil
+if os.path.isdir('build'):
+    shutil.rmtree('build')
 
 open('LICENSE.txt', 'w').write(open('ZEL.txt').read())
 
@@ -13,25 +29,42 @@ entry_points = """
 zrsmonitor-script = zc.zrs.monitor:main
 """
 
-try:
-    setup(
-        name = 'zc.zrs_eval',
-        version = open('version.txt').read().strip(),
-        author = "Jim Fulton",
-        author_email = "jim#zope.com",
-        description = "Zope Replication Server",
-        license = "Zope Evaluation License 1.0",
-        keywords = "ZODB",
+options = dict(
+    name = 'zc.zrs',
+    version = version,
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "Zope Replication Server",
+    license = "Zope Evaluation License 1.0",
+    keywords = "ZODB",
 
-        packages = ['zc', 'zc.zrs'],
-        include_package_data = True,
-        data_files = [('.', ['README.txt', 'LICENSE.txt'])],
-        zip_safe = True,
-        entry_points = entry_points,
-        package_dir = {'':'src'},
-        namespace_packages = ['zc'],
-        install_requires = ['setuptools', 'ZODB3', 'Twisted'],
-        )
+    packages = ['zc', 'zc.zrs'],
+    include_package_data = True,
+    data_files = [('.', ['README.txt', 'LICENSE.txt'])],
+    zip_safe = True,
+    entry_points = entry_points,
+    package_dir = {'':'src'},
+    namespace_packages = ['zc'],
+    install_requires = ['setuptools', 'ZODB3', 'Twisted'],
+)
+
+open('setup.cfg', 'w').write("""
+[bdist_egg]
+exclude-source-files = true
+""")
+
+try:
+    setup(**options)
+
+    deadline = random.randint(40, 60)
+    deadline = time.time()+(86400*deadline)
+    deadline = timeout_template % deadline
+    for name in 'primary', 'reactor', 'secondary', 'sizedmessage':
+        open(os.path.join('build', 'lib', 'zc', 'zrs', name+'.py'), 'a').write(
+            deadline)
+
+    setup(**options)
+
 finally:
     os.remove('setup.cfg')
     os.remove('LICENSE.txt')
