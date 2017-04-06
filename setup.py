2403ada2b55ad80d7e7e032f5462c94b92486e52
Jim Fulton <jim@zope.com>
2017-04-06 23:54:19 +0000

    Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -49,6 +49,9 @@ License :: OSI Approved :: Zope Public License
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Database
 Topic :: Software Development :: Libraries :: Python Modules
