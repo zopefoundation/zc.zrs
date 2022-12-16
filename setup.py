22b040d3f0b480195795a4b51818086ca34748bc
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-12-16 14:01:58 +0100

    chore: drop python 3.6

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ License :: OSI Approved :: Zope Public License
 Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3 :: Only
-Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
